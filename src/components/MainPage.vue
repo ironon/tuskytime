104bb8ddd8ab6eb9928bad873d2637e029cf88b9
David Macpherson <david.allan.macpherson@gmail.com>
2023-05-07 13:04:40 -0400

    deleted stuff

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
  
-    import { getAnalytics } from "firebase/analytics";
-    import {initializeApp} from "firebase/app"
-    import TuskyTime from "./TuskyTime.vue";
-    const firebaseConfig = {
-        apiKey: "AIzaSyA62gZmAbAqVdl7ySW9tZlKvIDUBi71ydQ",
-        authDomain: "tuskytime.firebaseapp.com",
-        databaseURL: "https://tuskytime-default-rtdb.firebaseio.com",
-        projectId: "tuskytime",
-        storageBucket: "tuskytime.appspot.com",
-        messagingSenderId: "462748644600",
-        appId: "1:462748644600:web:77801fadbdfac90f37f70d",
-        measurementId: "G-RHMV5QC0XJ"
-    };
-    const app = initializeApp(firebaseConfig)
+    // import { getAnalytics } from "firebase/analytics";
+    // import {initializeApp} from "firebase/app"
+    // import TuskyTime from "./TuskyTime.vue";
+    // const firebaseConfig = {
+    //     apiKey: "AIzaSyA62gZmAbAqVdl7ySW9tZlKvIDUBi71ydQ",
+    //     authDomain: "tuskytime.firebaseapp.com",
+    //     databaseURL: "https://tuskytime-default-rtdb.firebaseio.com",
+    //     projectId: "tuskytime",
+    //     storageBucket: "tuskytime.appspot.com",
+    //     messagingSenderId: "462748644600",
+    //     appId: "1:462748644600:web:77801fadbdfac90f37f70d",
+    //     measurementId: "G-RHMV5QC0XJ"
+    // };
+    // const app = initializeApp(firebaseConfig)
 
-    const analytics = getAnalytics(app);
+    // const analytics = getAnalytics(app);
     
 </script>
 
