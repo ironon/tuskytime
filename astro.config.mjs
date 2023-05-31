21388f6a4587230d056979039de30e9f2227544e
David Macpherson <david.allan.macpherson@gmail.com>
2023-05-31 11:30:29 -0400

    IT WAS THE STUPID CONFIG THE ENTIRE TIME AHHHHHH

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions'
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
